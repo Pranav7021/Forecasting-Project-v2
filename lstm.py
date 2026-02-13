6c50d64e26ae616d5c4132389f18b4cac574b7bd
Pranav7021 <71223307+Pranav7021@users.noreply.github.com>
2026-02-13 11:07:52 -0500

    Fixed lstm.py

--- lstm.py
+++ lstm.py
@@ -4,7 +4,15 @@ class LSTM(torch.nn.Module):
 	def __init__(self, input_size, hidden_size, num_layers, batch_first):
 		super(LSTM, self).__init__()
 		self.lstm_layer = torch.nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=batch_first)
+		self.dnn = torch.nn.Linear(seq_len*hidden_size, 1)
+
+		self.num_param = 0
+		for p in self.parameters():
+			self.num_param += p.numel()
 
 	def forward(self, x):
-		x = self.lstm_layer(x)
+		x = x.view([1, -1, 1])
+		x, _ = self.lstm_layer(x)
+		x = torch.flatten(x)
+		x = self.dnn(x)
 		return x
