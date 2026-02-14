ef8333e949c5e96ba89721021f4b4820ba578b84
Pranav7021 <71223307+Pranav7021@users.noreply.github.com>
2026-02-14 01:17:22 -0500

    Updated train_lstm_cf.py with cf code

--- train_lstm_cf.py
+++ train_lstm_cf.py
@@ -2,12 +2,29 @@ from load_data import CovidData
 from lstm import LSTM
 from train_model import train_model
 from torch.utils.data import DataLoader
+import time
 
 covid_data_obj = CovidData() 
 covid_data = DataLoader(covid_data_obj, batch_size=1, shuffle=False)
 
+# initialize the LSTM model
 lstm = LSTM(1, 10, 1, True, 200)
 print(f"***Initialized LSTM with {lstm.num_param} parameters***")
 
-# trains the LSTM and saves the parameters in a file
+# train the LSTM and save the parameters in a file
+stime = time.perf_counter()
 train_model(lstm, 20, covid_data, "trial_lstm.param")
+etime = time.perf_counter()
+
+print(f"***LSTM finished training in {etime-stime}s***")
+
+# initialize the CF model
+cf = CF()
+print(f"***Initialized CF with {cf.num_param} parameters***")
+
+# train the CF and save the parameters in a file
+stime = time.perf_counter()
+train_model(cf, 20, covid_data, "trial_cf.param")
+etime = time.perf_counter()
+
+print(f"***CF finished training in {etime-stime}s***")
