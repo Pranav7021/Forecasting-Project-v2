2d251046b671419e15d286aac90f6d7d4f489b88
Pranav <p9pathak@uwaterloo.ca>
2026-02-19 10:25:19 -0700

    Updated parameters for LSTM and CF training

--- train_lstm_cf.py
+++ train_lstm_cf.py
@@ -9,12 +9,12 @@ data_obj = TrainData()
 data = DataLoader(data_obj, batch_size=1, shuffle=False)
 
 # initialize the LSTM model
-lstm = LSTM(1, 30, 1, True, 200)
+lstm = LSTM(1, 30, 1, True, 100)
 print(f"***Initialized LSTM with {lstm.num_param} parameters***")
 
 # train the LSTM and save the parameters in a file
 stime = time.perf_counter()
-train_model(lstm, 40, data, "lstm.param")
+train_model(lstm, 80, data, "lstm.param")
 etime = time.perf_counter()
 
 print(f"***LSTM finished training in {etime-stime}s***")
@@ -25,7 +25,7 @@ print(f"***Initialized CF with {cf.num_param} parameters***")
 
 # train the CF and save the parameters in a file
 stime = time.perf_counter()
-train_model(cf, 75, data, "cf.param")
+train_model(cf, 120, data, "cf.param")
 etime = time.perf_counter()
 
 print(f"***CF finished training in {etime-stime}s***")
