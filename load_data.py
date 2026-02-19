67b0376f2444ab63346a53604f8b0f2df17d5bf8
Pranav <p9pathak@uwaterloo.ca>
2026-02-19 10:12:58 -0700

    Added NVDA stock testing data

--- load_data.py
+++ load_data.py
@@ -29,10 +29,36 @@ class TrainData(torch.utils.data.Dataset):
 		self.data_x = np.array(self.data_x, dtype=np.float32)
 		self.data_y = np.array(self.data_y, dtype=np.float32)
 		self.len = np.size(self.data_y)
-		print(f"***Initialized COVID training dataset with {self.len} examples***")
+		print(f"***Initialized training dataset with {self.len} examples***")
 
 	def __len__(self):
 		return self.len
 
 	def __getitem__(self, i):
 		return self.data_x[i], self.data_y[i]
+
+class StockData(torch.utils.data.Dataset):
+  def __init__(self):
+    self.nvda_data_path = "./data/NVDA.csv"
+    self.nvda_df = pd.read_csv(self.nvda_data_path)
+
+    self.close = pd.Series.to_numpy(self.nvda_df["close"])
+    self.close = (self.close - np.mean(self.close)) / np.std(self.close)
+    self.data_x = []
+    self.data_y = []
+    self.seq_len = 100
+
+    for i in range(len(self.close) - self.seq_len):
+      self.data_x.append(self.close[i: i+self.seq_len])
+      self.data_y.append(self.close[i+self.seq_len])
+
+    self.data_x = np.array(self.data_x, dtype=np.float32)
+    self.data_y = np.array(self.data_y, dtype=np.float32)
+    self.len = np.size(self.data_y)
+    print(f"***Initialized stock dataset with {self.len} examples***")
+
+  def __len__(self):
+    return self.len
+
+  def __getitem__(self, i):
+    return self.data_x[i], self.data_y[i]
