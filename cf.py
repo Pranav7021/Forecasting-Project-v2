852ae4c400c7b35c725c8fc1667d366eec449f89
Pranav <p9pathak@uwaterloo.ca>
2026-02-19 10:32:33 -0700

    Updated CF implementation

--- cf.py
+++ cf.py
@@ -10,9 +10,9 @@ class CF(torch.nn.Module):
 		self.max_pool1 = torch.nn.MaxPool1d(kernel_size=3, stride=1)
 
 		self.conv_on_5 = torch.nn.Conv1d(in_channels=24, out_channels=48, kernel_size=5)
-    	self.conv_on_10 = torch.nn.Conv1d(in_channels=12, out_channels=24, kernel_size=10)
-    	self.conv_on_25 = torch.nn.Conv1d(in_channels=12, out_channels=24, kernel_size=15)
-    	self.conv_on_50 = torch.nn.Conv1d(in_channels=12, out_channels=24, kernel_size=20)
+		self.conv_on_10 = torch.nn.Conv1d(in_channels=12, out_channels=24, kernel_size=10)
+		self.conv_on_25 = torch.nn.Conv1d(in_channels=12, out_channels=24, kernel_size=15)
+		self.conv_on_50 = torch.nn.Conv1d(in_channels=12, out_channels=24, kernel_size=20)
 		self.max_pool2 = torch.nn.MaxPool1d(kernel_size=4, stride=2)
 
 		self.dnn = torch.nn.Linear(768, 1)
@@ -23,12 +23,12 @@ class CF(torch.nn.Module):
 	def forward(self, x):
 		x1 = self.conv5(x[0,80:].view([1,1,-1]))
 		x2 = self.max_pool1(self.conv10(x[0,70:].view([1,1,-1])))
-    	x3 = self.max_pool1(self.conv25(x[0,50:].view([1,1,-1])))
-    	x4 = self.max_pool1(self.conv50(x.view([1, 1, -1])))
+		x3 = self.max_pool1(self.conv25(x[0,50:].view([1,1,-1])))
+		x4 = self.max_pool1(self.conv50(x.view([1, 1, -1])))
 
-    	x1 = self.max_pool2(self.conv_on_5(x1))
-    	x2 = self.max_pool2(self.conv_on_10(x2))
-    	x3 = self.max_pool2(self.conv_on_25(x3))
+		x1 = self.max_pool2(self.conv_on_5(x1))
+		x2 = self.max_pool2(self.conv_on_10(x2))
+		x3 = self.max_pool2(self.conv_on_25(x3))
 		x4 = self.max_pool2(self.conv_on_50(x4))
 		
 		x = torch.cat((torch.flatten(x1), torch.flatten(x2), torch.flatten(x3), torch.flatten(x4)))
