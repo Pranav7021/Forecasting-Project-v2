3fd9d6e8aa45c696d00c04fb99ac3c79dad8be04
Pranav <p9pathak@uwaterloo.ca>
2026-05-07 16:51:18 -0600

    Updated CF and data loading code

--- cf.py
+++ cf.py
@@ -1,36 +1,75 @@
 import torch
+import random
+
+class ConvLayer(torch.nn.Module):
+	def __init__(self, seq_len, nfeatures, nhead, dropout=0):
+		super(ConvLayer, self).__init__()
+		
+		self.seq_len = seq_len
+		self.nfeatures = nfeatures
+		self.nhead = nhead
+		self.dropout = dropout
+
+		self.convs = torch.nn.ModuleList()
+		self.dnns = torch.nn.ModuleList()
+		self.max_pool = torch.nn.MaxPool2d(kernel_size=(2,1), stride=(2,1))
+
+		for i in range(nhead):
+			self.convs.append(torch.nn.Conv1d(in_channels=nfeatures, out_channels=nfeatures*2, kernel_size=(i+2)**2))
+			self.dnns.append(torch.nn.Linear(in_features = nfeatures*2*(seq_len-(i+2)**2+1), out_features = nfeatures*seq_len//nhead))
+
+	def forward(self, x):
+		nx = []
+		for i in range(self.nhead):
+			y = self.convs[i](x)
+			y = torch.flatten(y)
+			y = self.dnns[i](y)
+			nx.append(y)
+
+		x = torch.cat(nx).view((1, self.nfeatures, self.seq_len))
+		x = self.max_pool(x)
+		return x.view((self.nfeatures//2, self.seq_len))
 
 class CF(torch.nn.Module):
-	def __init__(self):
+	def __init__(self, num_att_layers, dim):
 		super(CF, self).__init__()
-		self.conv5 = torch.nn.Conv1d(in_channels=1, out_channels=24, kernel_size=5)
-		self.conv10 = torch.nn.Conv1d(in_channels=1, out_channels=12, kernel_size=10)
-		self.conv25 = torch.nn.Conv1d(in_channels=1, out_channels=12, kernel_size=25)
-		self.conv50 = torch.nn.Conv1d(in_channels=1, out_channels=12, kernel_size=50)
-		self.max_pool1 = torch.nn.MaxPool1d(kernel_size=3, stride=1)
 
-		self.conv_on_5 = torch.nn.Conv1d(in_channels=24, out_channels=48, kernel_size=5)
-		self.conv_on_10 = torch.nn.Conv1d(in_channels=12, out_channels=24, kernel_size=10)
-		self.conv_on_25 = torch.nn.Conv1d(in_channels=12, out_channels=24, kernel_size=15)
-		self.conv_on_50 = torch.nn.Conv1d(in_channels=12, out_channels=24, kernel_size=20)
-		self.max_pool2 = torch.nn.MaxPool1d(kernel_size=4, stride=2)
+		if torch.cuda.is_available():
+			device = torch.device("cuda")
+		elif torch.backends.mps.is_available():
+			device = torch.device("mps")
+		else:
+			device = torch.device("cpu")
 
-		self.dnn = torch.nn.Linear(768, 1)
+		self.dim = dim
+		self.emb = torch.nn.Embedding(num_embeddings=100, embedding_dim=dim)
+		
+		self.num_att_layers = num_att_layers	
+
+		self.conv_layers = torch.nn.ModuleList()
+
+		for i in range(num_att_layers):
+			sz = dim//pow(2, i)
+			self.conv_layers.append(ConvLayer(seq_len=100, nfeatures=sz, nhead=5))
+
+		self.inds = []
+		for i in range(100):
+			self.inds.append([i])
+		
+		self.inds = torch.tensor(self.inds).to(device)
+
+		self.dnn = torch.nn.Linear((self.dim//pow(2, self.num_att_layers))*100, 1)
+
 		self.num_param = 0
 		for p in self.parameters():
 			self.num_param += p.numel()
 
-	def forward(self, x):
-		x1 = self.conv5(x[0,80:].view([1,1,-1]))
-		x2 = self.max_pool1(self.conv10(x[0,70:].view([1,1,-1])))
-		x3 = self.max_pool1(self.conv25(x[0,50:].view([1,1,-1])))
-		x4 = self.max_pool1(self.conv50(x.view([1, 1, -1])))
+	def forward(self, x): # x is (1, 100)
+		embeddings = self.emb(self.inds).view((100, self.dim)).T
+		x = torch.mul(embeddings, x)
 
-		x1 = self.max_pool2(self.conv_on_5(x1))
-		x2 = self.max_pool2(self.conv_on_10(x2))
-		x3 = self.max_pool2(self.conv_on_25(x3))
-		x4 = self.max_pool2(self.conv_on_50(x4))
-		
-		x = torch.cat((torch.flatten(x1), torch.flatten(x2), torch.flatten(x3), torch.flatten(x4)))
-		x = self.dnn(x)
-		return x
+		for i in range(self.num_att_layers):
+			x = self.conv_layers[i](x)
+
+		x = torch.flatten(x)
+		return self.dnn(x)
--- load_data.py
+++ load_data.py
@@ -11,28 +11,28 @@ class TrainData(torch.utils.data.Dataset):
 		else:
 			device = torch.device("cpu")
 
-		self.covid_data_path = "./data/Covid_19_Data.csv"
-		self.covid_df = pd.read_csv(self.covid_data_path)
 		self.aapl_data_path = "./data/AAPL.csv"
 		self.aapl_df = pd.read_csv(self.aapl_data_path)
+		self.spy_data_path = "./data/SPY.csv"
+		self.spy_df = pd.read_csv(self.spy_data_path)
 	
-		self.confirmed = pd.Series.to_numpy(self.covid_df["Confirmed"])
-		self.confirmed = (self.confirmed - np.mean(self.confirmed)) / np.std(self.confirmed)
-		self.close = pd.Series.to_numpy(self.aapl_df["close"])
-		self.close = (self.close - np.mean(self.close)) / np.std(self.close)
+		self.close1 = pd.Series.to_numpy(self.aapl_df["close"])
+		self.close1 = (self.close1 - np.mean(self.close1)) / np.std(self.close1)
+		self.close2 = pd.Series.to_numpy(self.spy_df["close"])
+		self.close2 = (self.close2 - np.mean(self.close2)) / np.std(self.close2)
 
 		self.data_x = []
 		self.data_y = []
 		self.seq_len = 100
 
-		for i in range(len(self.confirmed) - self.seq_len):
-			self.data_x.append(self.confirmed[i: i+self.seq_len])
-			self.data_y.append(self.confirmed[i+self.seq_len])
+		for i in range(len(self.close1) - self.seq_len):
+			self.data_x.append(self.close1[i: i+self.seq_len])
+			self.data_y.append(self.close1[i+self.seq_len])
 
-		for i in range(len(self.close) - self.seq_len):
-			self.data_x.append(self.close[i: i+self.seq_len])
-			self.data_y.append(self.close[i+self.seq_len])
-
+		for i in range(len(self.close2) - self.seq_len):
+			self.data_x.append(self.close2[i: i+self.seq_len])
+			self.data_y.append(self.close2[i+self.seq_len])
+		
 		self.data_x = np.array(self.data_x, dtype=np.float32)
 		self.data_x = torch.tensor(self.data_x, device=device)
 		self.data_y = np.array(self.data_y, dtype=np.float32)
@@ -48,22 +48,50 @@ class TrainData(torch.utils.data.Dataset):
 
 class StockData(torch.utils.data.Dataset):
 	def __init__(self):
+		if torch.cuda.is_available():
+			device = torch.device("cuda")
+		elif torch.backends.mps.is_available():
+			device = torch.device("mps")
+		else:
+			device = torch.device("cpu")
+
 		self.nvda_data_path = "./data/NVDA.csv"
 		self.nvda_df = pd.read_csv(self.nvda_data_path)
+		self.googl_data_path = "./data/GOOGL.csv"
+		self.googl_df = pd.read_csv(self.googl_data_path)
+		self.pins_data_path = "./data/PINS.csv"
+		self.pins_df = pd.read_csv(self.pins_data_path)
 
-		self.close = pd.Series.to_numpy(self.nvda_df["close"])
-		self.close = (self.close - np.mean(self.close)) / np.std(self.close)
+		self.close1 = pd.Series.to_numpy(self.nvda_df["close"])
+		self.close1 = (self.close1 - np.mean(self.close1)) / np.std(self.close1)
+
+		self.close2 = pd.Series.to_numpy(self.googl_df["close"])
+		self.close2 = (self.close2 - np.mean(self.close2)) / np.std(self.close2)
+
+		self.close3 = pd.Series.to_numpy(self.pins_df["close"])
+		self.close3 = (self.close3 - np.mean(self.close3)) / np.std(self.close3)
+
 		self.data_x = []
 		self.data_y = []
 		self.seq_len = 100
 
-		for i in range(len(self.close) - self.seq_len):
-			self.data_x.append(self.close[i: i+self.seq_len])
-			self.data_y.append(self.close[i+self.seq_len])
+		for i in range(len(self.close1) - self.seq_len):
+			self.data_x.append(self.close1[i: i+self.seq_len])
+			self.data_y.append(self.close1[i+self.seq_len])
 
+		for i in range(len(self.close2) - self.seq_len):
+			self.data_x.append(self.close2[i: i+self.seq_len])
+			self.data_y.append(self.close2[i+self.seq_len])
+		
+		for i in range(len(self.close3) - self.seq_len):
+			self.data_x.append(self.close3[i: i+self.seq_len])
+			self.data_y.append(self.close3[i+self.seq_len])
+		
 		self.data_x = np.array(self.data_x, dtype=np.float32)
+		self.data_x = torch.tensor(self.data_x, device=device)
 		self.data_y = np.array(self.data_y, dtype=np.float32)
 		self.len = np.size(self.data_y)
+		self.data_y = torch.tensor(self.data_y, device=device)
 		print(f"***Initialized stock dataset with {self.len} examples***")
 
 	def __len__(self):
